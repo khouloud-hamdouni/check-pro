97a3e2666be8a09670ddce9486b586470523f416
khouloud-hamdouni <khouloudhamdouni20@gmail.com>
2023-02-11 15:56:44 +0100

    first commit

---
+++ check.js
@@ -0,0 +1,16 @@
+let vect_1 =[3,-5,4]
+let vect_2=[2,6,5]
+let ps=[]
+ dot_product(vect_1, vect_2)
+{
+
+    let ps = 0;
+
+
+    for (let i = 0; i < n; i++)
+        ps = ps+ vect_1[i] * vect_2[i];
+    return ps;
+}
+
+
+
